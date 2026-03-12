429b36131449af962f020fdc2b3f64b47aa67690
kdkchy <kdkchy@gmail.com>
2026-03-12 18:11:59 +0800

    feat: initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import vue from "@astrojs/vue";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [vue()],
+
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+      },
+    },
+  },
+});
---
+++ src/lib/axios.ts
@@ -0,0 +1,36 @@
+import Axios from "axios";
+import Cookies from "js-cookie";
+
+const api = Axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("token");
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (
+      error.response.status === 401 &&
+      !error.config.url.includes("/auth/login")
+    ) {
+      Cookies.remove("token");
+      localStorage.removeItem("user");
+      window.location.href = "/auth/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export { api };
